81c019cb1e101922170beb357af1c334ab07dd29
gaidardzhiev <1v4n@protonmail.com>
2026-02-08 11:20:37 +0000

    Sun Feb  8 11:20:35 AM UTC 2026

---
+++ new_prog.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include "snail.h"
+
+#define M "\x75\x73\x61\x67\x65\x3a\x20\x25\x73\x20\x3c\x73\x63\x72\x69\x70\x74\x2e\x73\x6e\x6c\x3e\n"
+
+#define U if(!(_>>1)){printf(M,__[0]);return 1;}
+
+#define I(F,S,N) (F=fopen(__[_-1],"\x72\x62"),fseek(F,0,!!_+!!_),N=ftell(F),rewind(F),S=calloc(N+1,1),fread(S,sizeof(char),N,F),fclose(F))
+
+#define J(c,t,e) switch(c){case 0:t;break;default:e;}
+
+int main(int _,char**__){
+    U;
+    FILE*f;int n;
+    I(f,s,n);
+    q=0;nt();
+    A*h=0,**p=&h;
+    J(is('{'), while(!is(Q)){*p=S();p=&(*p)->n;}, h=K());
+    H*g=ne(NULL);
+    df(g,"outn",(V){0,(A*)1,0});
+    blk(h,g);
+    return 0;
+}
