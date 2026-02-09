dbd0f82fb649a1afce4f5a672b7e3d6697ce6f27
gaidardzhiev <1v4n@protonmail.com>
2026-02-09 06:53:52 +0000

    Mon Feb  9 06:53:51 AM UTC 2026

--- prog.c
+++ prog.c
@@ -4,4 +4,9 @@
 #include<ctype.h>
 #include "snail.h"
 
-int main(int _,char**__){if (_ < 2) {printf("\x75\x73\x61\x67\x65\x3a\x20\x25\x73\x20\x3c\x73\x63\x72\x69\x70\x74\x2e\x73\x6e\x6c\x3e\n",__[0]);return 1;}FILE*f=fopen(__[1],"rb");fseek(f,0,2);int n=ftell(f);rewind(f);s=malloc(n+1);fread(s,1,n,f);s[n]=0;fclose(f);q=0;nt();A*h=NULL,**p=&h;if(is('{'))h=K();else while(!is(Q)){*p=S();p=&(*p)->n;}H*g=ne(NULL);df(g,"outn",(V){0,(A*)1,0});blk(h,g);return 0;}
+#define ___ "\x75\x73\x61\x67\x65\x3a\x20\x25\x73\x20\x3c\x73\x63\x72\x69\x70\x74\x2e\x73\x6e\x6c\x3e\n"
+#define ____ if(!(_>>1)){printf(___,__[0]);return 1;}
+#define I(F,S,N) (F=fopen(__[_-1],"\x72\x62"),fseek(F,0,!!_+!!_),N=ftell(F),rewind(F),S=calloc(N+1,1),fread(S,sizeof(char),N,F),fclose(F))
+#define J(c,t,e) switch(c){case 0:t;break;default:e;}
+
+int main(int _,char**__){____;FILE*f;int n;I(f,s,n);q=0;nt();A*h=0,**p=&h;J(is('{'), while(!is(Q)){*p=S();p=&(*p)->n;}, h=K());H*g=ne(NULL);df(g,"outn",(V){0,(A*)1,0});blk(h,g);return 0;}
